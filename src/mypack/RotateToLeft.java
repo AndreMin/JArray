f644f69291636cefa365cbf57a13fcaa9ac2d923
AndreMin <minok.mal@gmail.com>
2020-07-11 15:08:01 +0300

    rotate to left array (apply cycle)

--- src/mypack/RotateToLeft.java
+++ src/mypack/RotateToLeft.java
@@ -1,4 +1,24 @@
 package mypack;
 
 public class RotateToLeft {
+    public static void main(String[] args) {
+        rotateToLeft();
+    }
+
+    public static void rotateToLeft() {
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = 2;
+        for (int i = 0; i < n; i++) {
+            int first = arr[0];
+            int j;
+            for (j = 0; j < arr.length - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            arr[j] = first;
+        }
+
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+    }
 }
